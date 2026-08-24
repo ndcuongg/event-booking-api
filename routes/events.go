31a25f7a25e1366d1d61060f8f1d82ee735cca83
Nguyen Dang Cuong <dangcuong201004@gmail.com>
2026-08-24 22:22:45 +0700

    feat: implement authentication middleware for event routes

--- routes/events.go
+++ routes/events.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/ndcuongg/event-booking-api.git/models"
-	"github.com/ndcuongg/event-booking-api.git/utils"
 )
 
 func getEvents(context *gin.Context) {
@@ -20,26 +19,16 @@ func getEvents(context *gin.Context) {
 
 func createEvent(context *gin.Context) {
 	// Authentication Required
-	token := context.Request.Header.Get("Authorization")
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
-		return
-	}
-
-	userId, err := utils.VerifyToken(token)
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
-		return
-	}
 
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Couldn't parse request data!"})
 		return
 	}
 
+	userId := context.GetInt64("userId")
 	event.UserID = userId
 
 	err = event.Save()
